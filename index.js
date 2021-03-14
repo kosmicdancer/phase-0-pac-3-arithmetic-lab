d0942f232fc6bf4d1771c4ed5aa3dfcbbab0b51a
Kyoko <kyokoconnect@gmail.com>
2021-03-14 17:27:48 -0400

    could not figure out errors commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+    return (a + b)
+}
+
+function subtract(a,b) {
+    return (a - b)
+}
+
+function multiply(a, b) {
+    return (a * b)
+}
+
+function divide(a, b) {
+    return (a / b)
+}
+
+function increment(n) {
+    return (n+=1)
+}
+
+function decrement(n) {
+    return (n-=1)
+}
+
+function parceInt(string, base10) {
+    return (number);
+}
+
+function parseFloat(string) {
+    return (number);
+}
+
+function makeInt(string) {
+    return number = (parceInt(string, base10))
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
